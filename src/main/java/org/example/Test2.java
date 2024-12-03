c0aac497d13900285ef40d1eff4efbaa9b41042f
Artem Garanin <artemgar.garanin@gmail.com>
2024-12-03 10:23:39 +0300

    branch test2 commit 2

--- src/main/java/org/example/Test2.java
+++ src/main/java/org/example/Test2.java
@@ -4,4 +4,11 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("Hello TEST 2");
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+
 }
